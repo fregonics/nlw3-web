a226e2846bbb4c8dfe93523ad67109c9a70b6b27
Luiz <luizgustavofcsg@hotmail.com>
2020-12-03 06:43:48 -0300

    final commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CreateOrphanage from './pages/CreateOrphanage';
 import Landing from './pages/Landing';
+import Orphanage from './pages/Orphanage';
 import OrphanagesMap from './pages/OrphanagesMap';
 
 
@@ -10,6 +12,8 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanagesMap} />
+                <Route path="/orphanages/:id" component={Orphanage} />
+                <Route path="/orphanage/create" component={CreateOrphanage} />
             </Switch>
         </BrowserRouter>
     );
